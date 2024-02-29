3b9992feee5344735a83f5bd1d3148dbeec3bf6b
Peter <pja4@njit.edu>
2024-02-29 12:02:33 -0500

    completed lab

--- app/src/main/java/com/codepath/articlesearch/BestSellerBooksFragment.kt
+++ app/src/main/java/com/codepath/articlesearch/BestSellerBooksFragment.kt
@@ -22,7 +22,8 @@ import org.json.JSONObject
 // --------------------------------//
 // CHANGE THIS TO BE YOUR API KEY  //
 // --------------------------------//
-private const val API_KEY = BuildConfig.API_KEY
+//private const val API_KEY = BuildConfig.API_KEY2
+private const val API_KEY = "HUNj4xijZfAwn6WXADdiE7wiiBMCjDhU"
 /*
  * The class for the only fragment in the app, which contains the progress bar,
  * recyclerView, and performs the network calls to the NY Times API.
